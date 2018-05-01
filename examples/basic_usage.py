1559a26155fd9037ded0211b5cf86f3c655dccb7
Timothy Crosley <timothy.crosley@gmail.com>
2018-05-01 09:09:29 -0700

    Version 0.0.4 - easy toggle support

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,15 +1,13 @@
 import time
 
 import hug
-from hug_explainable import Explainable
+from hug_explainable import explainable_toggle
 
 
 @hug.get()
-def my_endpoint(hug_timer, explain: Explainable=False):
+def my_endpoint(hug_timer, explain: explainable_toggle=explainable_toggle(False)):
     results = {'took': hug_timer}
     with explain('Sleeping for allocated time:', 1):
         time.sleep(1)
     explain.insert_into(results)
     return results
-
-    
