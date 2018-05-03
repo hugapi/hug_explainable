aae1d36a61fb0e6e31af9182feeff05978f4ba14
Timothy Crosley <tcrosley@domaintools.com>
2018-05-03 13:18:22 -0700

    Bump vresion to 0.0.8

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ except (IOError, ImportError, OSError, RuntimeError):
    readme = ''
 
 setup(name='hug_explainable',
-      version='0.0.7',
+      version='0.0.8',
       description='Provides an on demand context manager that makes it easy to profile and explain code blocks / paths within hug.',
       long_description=readme,
       author='Timothy Crosley',
